82194145925b26e2e7fd9307749c68396e8de966
zbz <nnzbz@21cn.com>
2018-09-20 11:42:21 +0800

    * mybatis添加生成countSelective的插件

---
+++ src/main/java/rebue/mbgx/generator/CountSelectiveElementGenerator.java
@@ -0,0 +1,77 @@
+package rebue.mbgx.generator;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.codegen.mybatis3.ListUtilities;
+import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
+import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
+
+import rebue.mbgx.util.BooleanUtil;
+
+public class CountSelectiveElementGenerator extends AbstractXmlElementGenerator {
+
+    @Override
+    public void addElements(XmlElement parentElement) {
+        XmlElement answer = new XmlElement("select"); //$NON-NLS-1$
+
+        answer.addAttribute(new Attribute("id", "countSelective")); //$NON-NLS-1$
+        answer.addAttribute(new Attribute("resultType", "int"));
+
+        String parameterType = introspectedTable.getBaseRecordType();
+        answer.addAttribute(new Attribute("parameterType", parameterType));
+
+        context.getCommentGenerator().addComment(answer);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("select count(1) ");
+
+        answer.addElement(new TextElement(sb.toString()));
+
+        sb.setLength(0);
+        sb.append("from "); //$NON-NLS-1$
+        sb.append(introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime());
+        answer.addElement(new TextElement(sb.toString()));
+
+        XmlElement dynamicElement = new XmlElement("where"); //$NON-NLS-1$
+        answer.addElement(dynamicElement);
+
+        // XmlElement trimElement = new XmlElement("trim"); //$NON-NLS-1$
+        // trimElement.addAttribute(new Attribute("prefix", "(")); //$NON-NLS-1$
+        // //$NON-NLS-2$
+        // trimElement.addAttribute(new Attribute("suffix", ")")); //$NON-NLS-1$
+        // //$NON-NLS-2$
+        // trimElement.addAttribute(new Attribute("prefixOverrides", "and"));
+        // //$NON-NLS-1$ //$NON-NLS-2$
+        // dynamicElement.addElement(trimElement);
+
+        for (IntrospectedColumn introspectedColumn : ListUtilities.removeGeneratedAlwaysColumns(introspectedTable.getNonPrimaryKeyColumns())) {
+            XmlElement isNotNullElement = new XmlElement("if"); //$NON-NLS-1$
+            sb.setLength(0);
+            sb.append(introspectedColumn.getJavaProperty());
+            sb.append(" != null"); //$NON-NLS-1$
+            if (introspectedColumn.getFullyQualifiedJavaType().getFullyQualifiedName().equals("java.lang.String")//
+                    && !BooleanUtil.isBooleanColumn(introspectedColumn)) {
+                sb.append(" and "); //$NON-NLS-1$
+                sb.append(introspectedColumn.getJavaProperty());
+                sb.append(" != ''"); //$NON-NLS-1$
+            }
+            isNotNullElement.addAttribute(new Attribute("test", sb.toString())); //$NON-NLS-1$
+            dynamicElement.addElement(isNotNullElement);
+
+            sb.setLength(0);
+            sb.append("and ");
+            sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            sb.append(" = "); //$NON-NLS-1$
+            sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
+
+            isNotNullElement.addElement(new TextElement(sb.toString()));
+        }
+
+        if (context.getPlugins().sqlMapSelectAllElementGenerated(answer, introspectedTable)) {
+            parentElement.addElement(answer);
+        }
+    }
+
+}
---
+++ src/main/java/rebue/mbgx/plugin/CountSelectivePlugin.java
@@ -0,0 +1,47 @@
+package rebue.mbgx.plugin;
+
+import java.util.List;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.Document;
+import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
+import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
+
+import rebue.mbgx.generator.CountSelectiveElementGenerator;
+import rebue.mbgx.generator.CountSelectiveMethodGenerator;
+
+/**
+ * 给Mapper及其XML文件加上countSelective()方法的插件
+ * TODO MBG : 判断如果本element已经被修改过，将不重复生成
+ * 
+ * @author zbz
+ */
+public class CountSelectivePlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> paramList) {
+        return true;
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        AbstractJavaMapperMethodGenerator methodGenerator = new CountSelectiveMethodGenerator();
+        methodGenerator.setContext(context);
+        methodGenerator.setIntrospectedTable(introspectedTable);
+        methodGenerator.addInterfaceElements(interfaze);
+        return super.clientGenerated(interfaze, topLevelClass, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+        AbstractXmlElementGenerator elementGenerator = new CountSelectiveElementGenerator();
+        elementGenerator.setContext(context);
+        elementGenerator.setIntrospectedTable(introspectedTable);
+        elementGenerator.addElements(document.getRootElement());
+        return super.sqlMapDocumentGenerated(document, introspectedTable);
+    }
+
+}
