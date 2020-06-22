4fc1f2aff5b8e5d09aa7d9e29f99e1aa8ffac91f
zbz <nnzbz@qq.com>
2020-06-22 10:56:27 +0800

    * 优化代码

--- src/main/java/rebue/mbgx/MybatisGeneratorWrap.java
+++ src/main/java/rebue/mbgx/MybatisGeneratorWrap.java
@@ -1,38 +1,37 @@
 package rebue.mbgx;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.api.ShellCallback;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
-
-import lombok.extern.slf4j.Slf4j;
 import rebue.mbgx.custom.ProgressCallbackEx;
 import rebue.mbgx.custom.ShellCallbackEx;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
 
 @Slf4j
 public class MybatisGeneratorWrap {
     /**
-     * @param overwrite
-     *            是否覆盖原来的文件，<br>
-     *            true，直接覆盖已经存在的原文件<br>
-     *            false，不覆盖原文件，但是会添加一个文件，是原来文件名+".1"(.2.3.4.5一直排下去)
-     * @param sPropFiles
-     *            properties文件的路径，如果有多个，可以用逗号分隔
+     * @param overwrite  是否覆盖原来的文件，<br>
+     *                   true，直接覆盖已经存在的原文件<br>
+     *                   false，不覆盖原文件，但是会添加一个文件，是原来文件名+".1"(.2.3.4.5一直排下去)
+     * @param sPropFiles properties文件的路径，如果有多个，可以用逗号分隔
      */
     public static void gen(final boolean overwrite, final String sPropFiles)
             throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
         // 获取类路径（配置文件在此路径下）
-        final String sClassPath = MybatisGeneratorWrap.class.getClassLoader().getResource("").getPath();
+        final String sClassPath = Objects.requireNonNull(MybatisGeneratorWrap.class.getClassLoader().getResource("")).getPath();
 
         // 读取属性文件
         final Properties properties = new Properties();
@@ -50,16 +49,15 @@ public class MybatisGeneratorWrap {
         generator.generate(new ProgressCallbackEx());
 
         if (warnings.size() > 0) {
-            final StringBuilder sb = new StringBuilder();
-            sb.append("\r\n********************************** 请注意: 警告有 ");
-            sb.append(warnings.size());
-            sb.append(" 条 **********************************");
+            String warn = "********************************** 请注意: 警告有 " + warnings.size() + " 条 **********************************";
+            log.warn(StringUtils.rightPad(warn, 180));
+
             for (final String warning : warnings) {
-                sb.append("\r\n");
-                sb.append(warning);
+                log.warn(StringUtils.rightPad(warning, 180));
             }
-            sb.append("\r\n********************************************************************");
-            log.warn(sb.toString());
+
+            warn = "****************************************************************************************";
+            log.warn(StringUtils.rightPad(warn, 180));
         }
     }
 
--- src/main/java/rebue/mbgx/custom/ProgressCallbackEx.java
+++ src/main/java/rebue/mbgx/custom/ProgressCallbackEx.java
@@ -1,58 +1,57 @@
 package rebue.mbgx.custom;
 
-import org.mybatis.generator.api.ProgressCallback;
-
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.mybatis.generator.api.ProgressCallback;
 
 /**
  * 用日志记录进度
- * 
- * @author zbz
  *
+ * @author zbz
  */
 @Slf4j
 public class ProgressCallbackEx implements ProgressCallback {
 
     @Override
     public void introspectionStarted(final int totalTasks) {
-        log.info("************************************************************************");
-        log.info("* MBGX开始执行任务: 总任务数-{}                                            *", totalTasks);
-        log.info("************************************************************************");
+        log.info(StringUtils.rightPad("*********************************************************", 180));
+        log.info(StringUtils.rightPad("MBGX开始执行任务: 总任务数-" + totalTasks, 180));
+        log.info(StringUtils.rightPad("*********************************************************", 180));
     }
 
     @Override
     public void generationStarted(final int totalTasks) {
-        log.info("************************************************************************");
-        log.info("* 开始生成代码: 任务数-{}                                                 *", totalTasks);
-        log.info("************************************************************************");
+        log.info(StringUtils.rightPad("*********************************************************", 180));
+        log.info(StringUtils.rightPad("开始生成代码: 任务数-" + totalTasks, 180));
+        log.info(StringUtils.rightPad("*********************************************************", 180));
     }
 
     @Override
     public void saveStarted(final int totalTasks) {
-        log.info("************************************************************************");
-        log.info("* 开始保存文件: 任务数-{}                                                 *", totalTasks);
-        log.info("************************************************************************");
+        log.info(StringUtils.rightPad("*********************************************************", 180));
+        log.info(StringUtils.rightPad("开始保存文件: 任务数-" + totalTasks, 180));
+        log.info(StringUtils.rightPad("*********************************************************", 180));
     }
 
     @Override
     public void startTask(final String taskName) {
-        log.info("************************************************************************");
-        log.info("* 开始执行任务: 任务名称-{}                                                *", taskName);
-        log.info("************************************************************************");
+        log.info(StringUtils.rightPad("*********************************************************", 180));
+        log.info(StringUtils.rightPad("开始执行任务: 任务名称-" + taskName, 180));
+        log.info(StringUtils.rightPad("*********************************************************", 180));
     }
 
     @Override
     public void done() {
-        log.info("************************************************************************");
-        log.info("* MBGX所有任务执行完成！！！                                               *");
-        log.info("************************************************************************");
+        log.info(StringUtils.rightPad("*********************************************************", 180));
+        log.info(StringUtils.rightPad("MBGX所有任务执行完成！！！", 180));
+        log.info(StringUtils.rightPad("*********************************************************", 180));
     }
 
     @Override
     public void checkCancel() throws InterruptedException {
-        log.info("************************************************************************");
-        log.info("* 检查是否取消                                                           *");
-        log.info("************************************************************************");
+        log.info(StringUtils.rightPad("*********************************************************", 180));
+        log.info(StringUtils.rightPad("检查是否取消", 180));
+        log.info(StringUtils.rightPad("*********************************************************", 180));
     }
 
 }
