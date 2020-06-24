c0a03c13b9773aa01da48de466401db6f1f66d10
zbz <nnzbz@qq.com>
2020-06-24 09:17:26 +0800

    * 优化代码

--- src/main/java/rebue/mbgx/plugin/DateTimeFormatPlugin.java
+++ src/main/java/rebue/mbgx/plugin/DateTimeFormatPlugin.java
@@ -1,14 +1,14 @@
 package rebue.mbgx.plugin;
 
+import java.sql.Types;
+import java.util.List;
+
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.Plugin;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
-
-import java.sql.Types;
-import java.util.List;
 
 /**
  * <pre>
@@ -26,8 +26,9 @@ public class DateTimeFormatPlugin extends PluginAdapter {
     }
 
     @Override
-    public boolean modelFieldGenerated(final Field field, final TopLevelClass topLevelClass, final IntrospectedColumn introspectedColumn, final IntrospectedTable introspectedTable,
-                                       final Plugin.ModelClassType modelClassType) {
+    public boolean modelFieldGenerated(final Field field, final TopLevelClass topLevelClass,
+            final IntrospectedColumn introspectedColumn, final IntrospectedTable introspectedTable,
+            final Plugin.ModelClassType modelClassType) {
         if (Types.DATE == introspectedColumn.getJdbcType()) {
             addDateAnnotations(field, topLevelClass);
         } else if (Types.TIME == introspectedColumn.getJdbcType()) {
@@ -42,30 +43,33 @@ public class DateTimeFormatPlugin extends PluginAdapter {
         topLevelClass.addImportedType("org.springframework.format.annotation.DateTimeFormat");
         topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
         field.addAnnotation("@DateTimeFormat(pattern = \"yyyy-MM-dd\")");
-        field.addAnnotation("@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = \"yyyy-MM-dd\", timezone = \"GMT+8\")");
+        field.addAnnotation(
+                "@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = \"yyyy-MM-dd\", timezone = \"GMT+8\")");
     }
 
     private void addTimeAnnotations(final Field field, final TopLevelClass topLevelClass) {
         topLevelClass.addImportedType("org.springframework.format.annotation.DateTimeFormat");
         topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
         field.addAnnotation("@DateTimeFormat(pattern = \"HH:mm:ss\")");
-        field.addAnnotation("@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = \"HH:mm:ss\", timezone = \"GMT+8\")");
+        field.addAnnotation(
+                "@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = \"HH:mm:ss\", timezone = \"GMT+8\")");
     }
 
     private void addDateTimeAnnotations(final Field field, final TopLevelClass topLevelClass) {
         topLevelClass.addImportedType("org.springframework.format.annotation.DateTimeFormat");
         topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
         field.addAnnotation("@DateTimeFormat(pattern = \"yyyy-MM-dd HH:mm:ss\")");
-        field.addAnnotation("@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = \"yyyy-MM-dd HH:mm:ss\", timezone = \"GMT+8\")");
+        field.addAnnotation(
+                "@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = \"yyyy-MM-dd HH:mm:ss\", timezone = \"GMT+8\")");
     }
 
-    private boolean isEndsWith(final String str, final String[] suffixs) {
-        for (final String suffix : suffixs) {
-            if (str.endsWith(suffix)) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    private boolean isEndsWith(final String str, final String[] suffixs) {
+//        for (final String suffix : suffixs) {
+//            if (str.endsWith(suffix)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
 }
