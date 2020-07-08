1519e2afda79788d667cc40b3d25cebcc6700a21
zbz <nnzbz@21cn.com>
2020-07-08 09:23:01 +0800

    * 修改组非主键不用必填

--- src/main/java/rebue/mbgx/plugin/Jsr303Plugin.java
+++ src/main/java/rebue/mbgx/plugin/Jsr303Plugin.java
@@ -23,44 +23,38 @@ public class Jsr303Plugin extends PluginAdapter {
     }
 
     @Override
-    public boolean modelFieldGenerated(final Field field, final TopLevelClass topLevelClass,
-            final IntrospectedColumn introspectedColumn, final IntrospectedTable introspectedTable,
+    public boolean modelFieldGenerated(final Field field, final TopLevelClass topLevelClass, final IntrospectedColumn introspectedColumn, final IntrospectedTable introspectedTable,
             final ModelClassType modelClassType) {
         // 如果字段要求非空
         if (!introspectedColumn.isNullable()) {
             topLevelClass.addImportedType("rebue.robotech.valid.AddGroup");
             topLevelClass.addImportedType("rebue.robotech.valid.ModifyGroup");
 
-            final String groups = "groups = "
-                    + (IntrospectedUtils.isPrimaryKey(introspectedColumn, introspectedTable) ? "ModifyGroup.class"
-                            : "{AddGroup.class, ModifyGroup.class}");
+            // 如果是主键就加入修改组，否则加入添加组(修改时必填主键，而添加时主键可由后端生成不用填)
+            final String groups = "groups = " + (IntrospectedUtils.isPrimaryKey(introspectedColumn, introspectedTable) ? "ModifyGroup.class" : "AddGroup.class");
             // 如果是字符串类型，添加 @NotBlank 注解
             if (introspectedColumn.isStringColumn()) {
                 topLevelClass.addImportedType("javax.validation.constraints.NotBlank");
-                field.addAnnotation("@NotBlank(" + groups + ", message = \""
-                        + RemarksUtils.getTitleByRemarks(introspectedColumn.getRemarks()) + "不能为空\")");
+                field.addAnnotation("@NotBlank(" + groups + ", message = \"" + RemarksUtils.getTitleByRemarks(introspectedColumn.getRemarks()) + "不能为空\")");
             }
             // 如果是非字符串类型，添加 @NotNull 注解
             else {
                 topLevelClass.addImportedType("javax.validation.constraints.NotNull");
-                field.addAnnotation("@NotNull(" + groups + ", message = \""
-                        + RemarksUtils.getTitleByRemarks(introspectedColumn.getRemarks()) + "不能为空\")");
+                field.addAnnotation("@NotNull(" + groups + ", message = \"" + RemarksUtils.getTitleByRemarks(introspectedColumn.getRemarks()) + "不能为空\")");
             }
         }
 
         // 如果是字符串且有长度限制，添加 @Length 注解
         if (introspectedColumn.isStringColumn() && introspectedColumn.getLength() > 0) {
             topLevelClass.addImportedType("org.hibernate.validator.constraints.Length");
-            field.addAnnotation("@Length(max = " + introspectedColumn.getLength() + ", message = \""
-                    + RemarksUtils.getTitleByRemarks(introspectedColumn.getRemarks()) + "的长度不能大于"
+            field.addAnnotation("@Length(max = " + introspectedColumn.getLength() + ", message = \"" + RemarksUtils.getTitleByRemarks(introspectedColumn.getRemarks()) + "的长度不能大于"
                     + introspectedColumn.getLength() + "\")");
         }
 
         // 如果是无符号类型，添加 @Min(...) 符号，限制为非负数
         if (introspectedColumn.getActualTypeName().contains("UNSIGNED")) {
             topLevelClass.addImportedType("javax.validation.constraints.PositiveOrZero");
-            field.addAnnotation("@PositiveOrZero(message = \""
-                    + RemarksUtils.getTitleByRemarks(introspectedColumn.getRemarks()) + "不能为负数\")");
+            field.addAnnotation("@PositiveOrZero(message = \"" + RemarksUtils.getTitleByRemarks(introspectedColumn.getRemarks()) + "不能为负数\")");
         }
 
         return true;
