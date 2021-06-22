bed4fc24724d144f34777571171a37f49125d9cb
nnzbz <nnzbz@qq.com>
2021-06-22 17:35:53 +0800

    + 自动生成给Mapper添加GetColumns方法

---
+++ src/main/java/rebue/mbgx/generator/GetColumnsMethodGenerator.java
@@ -0,0 +1,39 @@
+package rebue.mbgx.generator;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.JavaVisibility;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
+
+public class GetColumnsMethodGenerator extends AbstractJavaMapperMethodGenerator {
+
+    @Override
+    public void addInterfaceElements(final Interface interfaze) {
+        final FullyQualifiedJavaType      arrayItemJavaType = new FullyQualifiedJavaType("org.mybatis.dynamic.sql.BasicColumn");
+
+        final Set<FullyQualifiedJavaType> importedTypes     = new TreeSet<>();
+        importedTypes.add(arrayItemJavaType);
+
+        final Method method = new Method("getColumns");                                                  //$NON-NLS-1$
+        method.setDefault(true);
+        method.setVisibility(JavaVisibility.PUBLIC);
+
+        final FullyQualifiedJavaType returnType = new FullyQualifiedJavaType(arrayItemJavaType + "[]");
+        // returnType.addTypeArgument(arrayItemJavaType);
+        method.setReturnType(returnType);
+
+        method.addBodyLine("return selectList;");  //$NON-NLS-1$
+
+        context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
+
+        if (context.getPlugins().clientSelectAllMethodGenerated(method, interfaze, introspectedTable)) {
+            interfaze.addImportedTypes(importedTypes);
+            interfaze.addMethod(method);
+        }
+    }
+
+}
